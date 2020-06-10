938bb276f7d22278fd81a2aef723c5f29ba01610
Dustin Bates <dpbates13@gmail.com>
2020-06-10 17:17:55 -0500

    initial

---
+++ server.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+//const POKEDEX = require("./pokedex.json");
+const MOVIEDEX = require("./moviedex.json");
+const cors = require("cors");
+const helmet = require("helmet");
+
+console.log(process.env.API_TOKEN);
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  console.log("validate bearer token middleware");
+
+  next();
+});
+
+function handleGetMovie(req, res) {
+  let response = MOVIEDEX;
+
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter((movie) => {
+      return movie.avg_vote >= Number(req.query.avg_vote);
+    });
+  }
+
+  res.json(response);
+}
+
+app.get("/movie", handleGetMovie);
+
+app.listen(8000, () => {
+  console.log("server listening at 8000");
+});
